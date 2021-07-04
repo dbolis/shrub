9a80eb1fcf890e6fd8da7873b336d1a7e6d177fd
Daniel Bolis <daniel@Daniels-MacBook-Pro-2.local>
2021-07-04 10:12:01 -0400

    updated earthquake parser

--- earthquake_data/parse_usgs.py
+++ earthquake_data/parse_usgs.py
@@ -6,56 +6,58 @@ import dbCredentials
 
 # Connect to DB
 
-endpoint = dbCredentials.endpoint
-username = dbCredentials.username
-password = dbCredentials.password
-database_name = dbCredentials.database_name
+def updateDB(event=None, context=None):
+
+    endpoint = dbCredentials.endpoint
+    username = dbCredentials.username
+    password = dbCredentials.password
+    database_name = dbCredentials.database_name
 
 
-# Select the usgs ID
+    # Select the usgs ID
 
-connectionObject = pymysql.connect(host=endpoint, user=username, passwd=password, db=database_name)
+    connectionObject = pymysql.connect(host=endpoint, user=username, passwd=password, db=database_name)
 
-fetchCursor = connectionObject.cursor()
-fetchCursor.execute("SELECT id FROM Earthquakes")
-idColumn = fetchCursor.fetchall()
+    fetchCursor = connectionObject.cursor()
+    fetchCursor.execute("SELECT id FROM Earthquakes")
+    idColumn = fetchCursor.fetchall()
 
-usgsIds=set()
+    usgsIds=set()
 
-for id in idColumn:
-    usgsIds.add(id[0])
+    for id in idColumn:
+        usgsIds.add(id[0])
 
-# check USGS API
+    # check USGS API
 
-response = requests.get("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_hour.geojson")
-geo_json = json.loads(response.content)
-earthquakes = geo_json['features']
+    response = requests.get("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_hour.geojson")
+    geo_json = json.loads(response.content)
+    earthquakes = geo_json['features']
 
-print(usgsIds)
-# Parse and update DB
+    # Parse and update DB
 
-for item in earthquakes:
+    for item in earthquakes:
 
-    # check if item exists in table
-    if item["id"] not in usgsIds:
+        # check if item exists in table
+        if item["id"] not in usgsIds:
 
-    # parse relevant properties
-        print("~~~~~~~~~~~~~~~~~~~~~~~")
-        id = item["id"]
-        print(id)
-        place = item["properties"]["place"]
-        print(place)
-        magnitude = item["properties"]["mag"]
-        print(magnitude)
-        longitude = item["geometry"]["coordinates"][0]
-        print(longitude)
-        latitude = item["geometry"]["coordinates"][1]
-        print(latitude)
+        # parse relevant properties
+            print("~~~~~~~~~~~~~~~~~~~~~~~")
+            id = item["id"]
+            print(id)
+            place = item["properties"]["place"]
+            print(place)
+            magnitude = item["properties"]["mag"]
+            print(magnitude)
+            longitude = item["geometry"]["coordinates"][0]
+            print(longitude)
+            latitude = item["geometry"]["coordinates"][1]
+            print(latitude)
 
-    # update DB
+        # update DB
 
-        insertCursor = connectionObject.cursor()
-        insertStatement = "INSERT INTO Earthquakes (id,place,mag,longitude,latitude) VALUES (%s,%s,%s,%s,%s)"
-        insertCursor.execute(insertStatement,(id,place,magnitude,longitude,latitude))
-        connectionObject.commit()
+            insertCursor = connectionObject.cursor()
+            insertStatement = "INSERT INTO Earthquakes (id,place,mag,longitude,latitude) VALUES (%s,%s,%s,%s,%s)"
+            insertCursor.execute(insertStatement,(id,place,magnitude,longitude,latitude))
+            connectionObject.commit()
+
 
